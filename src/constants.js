9d6c35d33d3ed56e0b99da0cf9948ebac6caa807
Dante Tsang <waichuntwc@gmail.com>
2019-08-30 17:34:46 +0800

    Remove on9 west

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,8 @@ export default Object.freeze({
     TIMEZONE: 'Asia/Hong_Kong'
   },
   MESSAGE: {
-    TOP_TEN_TITLE: 'Top 10 濕鳩s in the last 7 days (last 上水 time):\n\n',
-    ALL_JUNG_TITLE: 'All 濕鳩s in the last 7 days (last 上水 time):\n\n',
+    TOP_TEN_TITLE: '最近七日十大濕鳩(last 上水 time):\n\n',
+    ALL_JUNG_TITLE: '全部濕鳩(last 上水 time):\n\n',
     LIMIT: 3800
   },
   CRON: {
@@ -15,21 +15,8 @@ export default Object.freeze({
     DB_CLEANUP_PATTERN: '0 0 0-17,19-23 * * *'
   },
   HELP: {
-    MESSAGE: '濕鳩bot' + '\n' +
+    MESSAGE: '我係濕鳩bot' + '\n' +
     '\n' +
-    'This bot is created for counting the number of message per participant in the group.' + '\n' +
-    '\n' +
-    'Rules:' + '\n' +
-    '1. Repeated messages will only be counted as ONE message.' + '\n' +
-    '2. Commands can only be used for ONCE per minute.' + '\n' +
-    '\n' +
-    'Commands:' + '\n' +
-    '/topten  show top ten 濕鳩s' + '\n' +
-    '/all19  show all 濕鳩s' + '\n' +
-    '/19help  show help message' + '\n' +
-    '\n' +
-    'Issue/Suggestion: https://github.com/dantetwc/telegram-jung2-bot/issues' + '\n' +
-    '\n' +
-    'May 1c7 be with you'
+    '關你撚事咩'
   }
 })
